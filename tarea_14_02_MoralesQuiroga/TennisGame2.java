68d3952259d2bb5f8933553e019ce5b972b15fdd
sergioMoralesPercella <sergio.morales@percella.com>
2016-02-14 02:21:54 -0300

    Deuce as a function

--- tarea_14_02_MoralesQuiroga/TennisGame2.java
+++ tarea_14_02_MoralesQuiroga/TennisGame2.java
@@ -10,14 +10,18 @@ public class TennisGame2 implements TennisGame
     private String player1Name;
     private String player2Name;
     private String[] scoreEquivalency = {"Love","Fifteen","Thirty","Forty"};
+    private String score = "";
     
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
+    public void isDeuce(){
+    	if (P1point==P2point && P1point>=3)
+    		score = "Deuce";
+    }
     public String getScore(){
-        String score = "";
         if (P1point == P2point && P1point < 4)
         {
             if (P1point==0)
@@ -28,9 +32,7 @@ public class TennisGame2 implements TennisGame
                 score = scoreEquivalency[P1point];
             score += "-All";
         }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
-        
+        isDeuce();
         if (P1point > 0 && P2point==0)
         {
             if (P1point==1)
