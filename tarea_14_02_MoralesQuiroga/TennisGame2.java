d8a04b8f03cb5a95270b118ca09c1c96fcb9caa1
sergioMoralesPercella <sergio.morales@percella.com>
2016-02-14 02:15:27 -0300

    Use an array of strings for the scores

---
+++ tarea_14_02_MoralesQuiroga/TennisGame2.java
@@ -0,0 +1,137 @@
+
+public class TennisGame2 implements TennisGame
+{
+	//TODO: Revisar los siguientes nombres de variables
+    public int P1point = 0;
+    public int P2point = 0;
+    
+    public String P1res = "";
+    public String P2res = "";
+    private String player1Name;
+    private String player2Name;
+    private String[] scoreEquivalency = {"Love","Fifteen","Thirty","Forty"};
+    
+    public TennisGame2(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public String getScore(){
+        String score = "";
+        if (P1point == P2point && P1point < 4)
+        {
+            if (P1point==0)
+                score = scoreEquivalency[P1point];
+            if (P1point==1)
+                score = scoreEquivalency[P1point];
+            if (P1point==2)
+                score = scoreEquivalency[P1point];
+            score += "-All";
+        }
+        if (P1point==P2point && P1point>=3)
+            score = "Deuce";
+        
+        if (P1point > 0 && P2point==0)
+        {
+            if (P1point==1)
+                P1res = scoreEquivalency[P1point];
+            if (P1point==2)
+                P1res = scoreEquivalency[P1point];
+            if (P1point==3)
+                P1res = scoreEquivalency[P1point];
+            
+            P2res = "Love";
+            score = P1res + "-" + P2res;
+        }
+        if (P2point > 0 && P1point==0)
+        {
+            if (P2point==1)
+                P2res = scoreEquivalency[P2point];
+            if (P2point==2)
+                P2res = scoreEquivalency[P2point];
+            if (P2point==3)
+                P2res = scoreEquivalency[P2point];
+            
+            P1res = "Love";
+            score = P1res + "-" + P2res;
+        }
+        
+        if (P1point>P2point && P1point < 4)
+        {
+            if (P1point==2)
+                P1res=scoreEquivalency[P1point];
+            if (P1point==3)
+                P1res=scoreEquivalency[P1point];
+            if (P2point==1)
+                P2res=scoreEquivalency[P2point];
+            if (P2point==2)
+                P2res=scoreEquivalency[P2point];
+            score = P1res + "-" + P2res;
+        }
+        if (P2point>P1point && P2point < 4)
+        {
+            if (P2point==2)
+                P2res=scoreEquivalency[P2point];
+            if (P2point==3)
+                P2res=scoreEquivalency[P2point];
+            if (P1point==1)
+                P1res=scoreEquivalency[P1point];
+            if (P1point==2)
+                P1res=scoreEquivalency[P1point];
+            score = P1res + "-" + P2res;
+        }
+        
+        if (P1point > P2point && P2point >= 3)
+        {
+            score = "Advantage player1";
+        }
+        
+        if (P2point > P1point && P1point >= 3)
+        {
+            score = "Advantage player2";
+        }
+        
+        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+        {
+            score = "Win for player1";
+        }
+        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        {
+            score = "Win for player2";
+        }
+        return score;
+    }
+    
+    public void SetP1Score(int number){
+        
+        for (int i = 0; i < number; i++)
+        {
+            P1Score();
+        }
+            
+    }
+    
+    public void SetP2Score(int number){
+        
+        for (int i = 0; i < number; i++)
+        {
+            P2Score();
+        }
+            
+    }
+    
+    public void P1Score(){
+        P1point++;
+    }
+    
+    public void P2Score(){
+        P2point++;
+    }
+
+    public void wonPoint(String player) {
+        if (player == "player1")
+            P1Score();
+        else
+            P2Score();
+    }
+}
